a391a8ef0ffcd2017d0e9ca4939b794b4b2a03a4
user-komeda <shigoto922@gmail.com>
2024-09-14 21:38:34 +0900

    #38 backend側を統合

--- .lefthook/pre-commit/lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pwd
-source lint.env
-qodana scan -e QODANA_TOKEN=$QODANA_TOKEN --fail-threshold 1 -s false
---
+++ NarouBackend/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,13 @@
+
+
+namespace NarouBackend.Controllers;
+
+using Bunit;
+using Xunit;
+
+
+public class WeatherForecastControllerTests : TestContext {
+    [Fact] public void weatherForecastControllerTest() {
+        Assert.True(true);
+    }
+}
