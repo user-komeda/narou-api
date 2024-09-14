209025253444cd9a0ea5c35f32c065f84f4c9a58
user-komeda <shigoto922@gmail.com>
2024-09-15 02:00:03 +0900

    #38 backend側を統合

---
+++ .lefthook/pre-commit/lint-front.sh
@@ -0,0 +1,2 @@
+source lint.env
+qodana scan -e QODANA_TOKEN=$QODANA_TOKEN --fail-threshold 1 -s false --source-directory NarouApp/ --config qodana-front.yaml  --property=qodana.net.solution=NarouApp.sln
--- .lefthook/pre-commit/lint.sh
+++
@@ -1,2 +0,0 @@
-source lint.env
-#qodana scan -e QODANA_TOKEN=$QODANA_TOKEN --fail-threshold 1 -s false --source-directory NarouApp/ --config qodana-backend.yaml  --property=qodana.net.solution=NarouBackend.sln
